c454e5c07e413c94a501a43ecfce348f625d044f
Ignacio Vizzo <ignacio@dexory.com>
2024-01-19 13:32:38 -0300

    Rebase from 'release/humble/ament_black'

--- test/test_cli.py
+++ test/test_cli.py
@@ -8,5 +8,5 @@ import pytest
 @pytest.mark.ament_black
 @pytest.mark.linter
 def test_ament_black():
-    rc = os.system("ament_black --help")
-    assert rc == 0, "ament-black python package not properly installed"
+    rc = os.system('ament_black --help')
+    assert rc == 0, 'ament-black python package not properly installed'
