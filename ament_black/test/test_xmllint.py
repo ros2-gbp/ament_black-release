58676cd02c25cce0f719442e2a5b031f3f633c55
Ignacio Vizzo <ignacio@dexory.com>
2024-01-19 13:32:31 -0300

    Rebase from 'upstream'

--- ament_black/test/test_xmllint.py
+++ ament_black/test/test_xmllint.py
@@ -12,12 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
 from ament_xmllint.main import main
+import pytest
 
 
 @pytest.mark.linter
 @pytest.mark.xmllint
 def test_xmllint():
     rc = main(argv=[])
-    assert rc == 0, "Found errors"
+    assert rc == 0, 'Found errors'
